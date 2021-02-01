7f6216f5232c6167156d33fe59c9bb1831f653e5
Igor <igorl1201@gmail.com>
2021-02-01 21:10:55 +0300

    commit to make pull

--- src/main/java/ru/stankin/practice/entity/PersonRepository.java
+++ src/main/java/ru/stankin/practice/entity/PersonRepository.java
@@ -1,7 +1,6 @@
 package ru.stankin.practice.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
