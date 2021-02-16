5d32aa564a97226315f610b79c1492f5278d5fe9
Dmtriy Kuchma <dk@kalite.ro>
2021-02-16 17:31:00 +0300

    Гениальные решения от не менее гениального меня

--- src/main/java/ru/stankin/practice/utils/Utils.java
+++ src/main/java/ru/stankin/practice/utils/Utils.java
@@ -1,5 +1,9 @@
 package ru.stankin.practice.utils;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 public class Utils {
     public static boolean isNumber(String str) {
         try {
@@ -8,5 +12,16 @@ public class Utils {
         } catch (NumberFormatException ex) {
             return false;
         }
+    }
+
+    public static String getDayOfWeek(int dayInMonth) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.DAY_OF_WEEK, dayInMonth);
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault());
+    }
+
+    public static int getDaysInCurrentMonth() {
+        Calendar calendar = new GregorianCalendar();
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 }
